ace6fb5ae97cf2d2c9502a75329f6bcc09f24ab7
Nathanieljla1 <nathanieljla@gmail.com>
2023-03-21 11:18:48 -0500

    Cleaned up import code

--- fspy_maya/__init__.py
+++ fspy_maya/__init__.py
@@ -1,10 +1,12 @@
 
-
 VERSION = (0, 9, 0)
 __version__ = '.'.join(map(str, VERSION))
 
 
 try:
+    #build distribution will fail, since maya modules aren't present.
+    #ad the toml gets the versioning info from the module.
     from .core import *
-except:    
+except ModuleNotFoundError:
     pass
+
